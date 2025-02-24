26a9c4214b09016b82a6a1bc06238dc3c78d8fb2
Ben Moelis <ben.moelis@kellogg.northwestern.edu>
2025-02-24 20:23:17 +0000

    Created controllers and views files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  get 'places/index'
+  get 'places/show'
+  get 'places/new'
+  get 'places/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
+
+  # Routes for places
+  resources :places do
+    resources :entries, only: [:new, :create]
+  end
+
+root "places#index"
 end
